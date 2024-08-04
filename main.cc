37907f0bf25ec13f1a2cae3bc131211f33f8c1b8
arsenio <2939286+milmana@users.noreply.github.com>
2024-08-04 19:23:19 -0400

    templates...

--- main.cc
+++ main.cc
@@ -10,6 +10,13 @@ class FOO {
 int main(int argc, char **argv) {
 
     vec3 foo{1., 2., 3.};
-
     std::cout << foo << std::endl;
+
+    foo = {
+        4.,
+        5.,
+        6.,
+    };
+std::cout << foo << std::endl;
+
 }
--- vec3.hh
+++ vec3.hh
@@ -6,14 +6,12 @@ struct vec3 {
     
     std::vector<T> v_;
 
-    vec3(T x, T y, T z) { v_ = {x,y,z}; };
+    vec3(T x, T y, T z) { v_ = {x,y,z}; }
 
-    const T& x() const { return v_[0];  }
-    const T& y() const { return v_[1];  }
-    const T& z() const { return v_[2];  }
-
+    T x() const { return v_[0]; }
+    T y() const { return v_[1]; }
+    T z() const { return v_[2]; }
 };
-
 
 template<typename T>
 std::ostream &operator<<(std::ostream &os, const vec3<T> &v)
